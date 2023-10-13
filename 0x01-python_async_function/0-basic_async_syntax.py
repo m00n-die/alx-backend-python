d312cbd4e853ce6d446303dd91724a02b341e93e
m00n-die <reginaldrock219@gmail.com>
2023-10-13 08:05:44 -0700

    pycodestyle validation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
 an asynchronous coroutine that takes in an integer argument
-(max_delay, with a default value of 10) named wait_random that waits for a random delay 
-between 0 and max_delay (included and float value) seconds and eventually returns it.
+(max_delay, with a default value of 10) named
+wait_random that waits for a random delay
+between 0 and max_delay (included and float
+value) seconds and eventually returns it.
 """
 import asyncio
 import random
