1f46165cbd46e1a5900b9d582d9029eb8e857a8d
Reginald Amuzu <95586757+m00n-die@users.noreply.github.com>
2023-10-13 14:36:55 +0000

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,11 +8,8 @@ import asyncio
 import random
 
 async def wait_random(max_delay=10):
+    """waits for random number of seconds before execution
+    completes"""
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time
-
-# Create an event loop
-loop = asyncio.get_event_loop()
-loop.run_until_complete(wait_random())
-loop.close()
