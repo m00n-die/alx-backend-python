dbbea7800f0090ef840bb76ae7fb87a4510d00fc
m00n-die <reginaldrock219@gmail.com>
2023-10-22 00:29:26 +0100

    quick fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns a tuple'''
     square: float = pow(v, 2)
     return (k, square)
