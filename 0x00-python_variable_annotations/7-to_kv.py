af9b4d03d441c08218e8377060bf356477d99e06
m00n-die <reginaldrock219@gmail.com>
2023-10-22 00:22:57 +0100

    quick fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """to kv"""
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
     '''returns a tuple'''
     square: float = pow(v, 2)
     return (k, square)
